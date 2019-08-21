fbd67013891498a1678e18d2f371dd6b535cfb4a
Kristian Eschenburg <keschenb@uw.edu>
2019-08-21 15:37:48 -0700

    aggregate correlations

--- bin/aggregate_correlation_all.py
+++ bin/aggregate_correlation_all.py
@@ -3,13 +3,22 @@ from fragmenter import RegionExtractor as re
 import argparse
 
 from connectopy import utilities as uti
+from connectopy import plotting
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--subject', help='Subjject ID', required=True, type=str)
-parser.add_argument('-conn', '--dir_conn', help='Connectopy directory.', required=True, type=str)
+parser.add_argument('-conn', '--conndir', help='Connectopy directory.', required=True, type=str)
 parser.add_argument('-hemi', '--hemisphere', help='Hemisphere.', required=True, type=str)
 parser.add_argument('-l', '--label', help='Label file.', required=True, type=str)
 
 args = parser.parse_args()
 
-[amp, exp] = plotting.csv2matrix(args.subject, args.hemisphere, dir_out)
+subject = args.subject
+dir_conn = args.conndir
+hemisphere = args.hemisphere
+
+R = re.Extractor(args.label)
+regions = R.map_regions()
+
+uti.s2t_correlations_aggregate(subject_id=subject, region_map=regions, 
+                               hemisphere=hemisphere, connectopy_dir=dir_conn)
