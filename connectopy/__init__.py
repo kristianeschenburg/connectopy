ab485837da335b0b72c53beb377e92e0c7089bb3
Kristian Eschenburg <keschenb@uw.edu>
2020-01-03 16:28:37 -0800

    update init

--- connectopy/__init__.py
+++ connectopy/__init__.py
@@ -3,4 +3,4 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from . import (l1_median, affine, write, similarity_mapping)
+from . import (l1_median, affine, write)
